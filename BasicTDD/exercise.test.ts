3f29ee134d14fceaef22ba7c15516d28c2677adc
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-15 09:14:23 +0100

    added more exercise

--- BasicTDD/exercise.test.ts
+++ BasicTDD/exercise.test.ts
@@ -1,7 +1,5 @@
 // exercise.test.ts
 
-import { add } from "./excercise"
-
 /* 
   1. Simple function testing
   Write a test for a function called `add` that adds two numbers. 
@@ -10,11 +8,6 @@ import { add } from "./excercise"
 */
 
 // TODO: Write your test here
-describe('add', () => {
-    test('should add two numbers', () => {
-        expect(add(1, 1)).toBe(2)
-    })
-})
 
 /* 
   2. Array manipulation testing
@@ -52,3 +45,52 @@ describe('add', () => {
 */
 
 // TODO: Write your test here
+
+/* 
+6. Complex Array manipulation testing
+
+Write a test for a function called findMostFrequent that finds the most frequent item in an array. The function should take an array as an argument, and return the most frequent item. If there are multiple items that appear the maximum number of times, the function can return any of them. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+7. Complex Object manipulation testing
+
+Write a test for a function called deepKeyValue that checks whether a key-value pair exists anywhere within a nested object, including at any depth. The function should take an object, a key, and a value as arguments, and return true if the key-value pair exists in the object or any of its nested objects, false otherwise. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  8. Array to Object conversion testing
+
+Write a test for a function called arrayToObject that takes an array of objects, each with a key and value property, and converts it into an object with those keys and values. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  9. Object to Array conversion testing
+
+Write a test for a function called objectToArray that takes an object and returns an array of its key-value pairs, each represented as a {key: ..., value: ...} object. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  10. Array sorting testing
+
+Write a test for a function called sortArray that sorts an array in ascending order. The function should take an array as an argument, and return a new array that contains the same elements but sorted. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  11. Object sorting testing
+
+Write a test for a function called sortObjectKeys that sorts an object by its keys in alphabetical order. The function should take an object as an argument, and return a new object that contains the same key-value pairs but with keys sorted. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
