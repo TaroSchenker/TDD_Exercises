eafc97f00595df74717166e81afec1501acdc4ce
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-15 09:29:40 +0100

    added more exercises

--- BasicTDD/exercise.test.ts
+++ BasicTDD/exercise.test.ts
@@ -110,3 +110,100 @@ describe("Object manipulation Testing", () => {
 */
 
 // TODO: Write your test here
+
+// exercise.test.ts
+
+/* 
+  1. Simple function testing
+  Write a test for a function called `add` that adds two numbers. 
+  The function should take two arguments and return their sum. 
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  2. Array manipulation testing
+  Write a test for a function called `includesValue` that checks whether an array includes a value. 
+  The function should take an array and a value as arguments, and return true if the value is in the array, false otherwise. 
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  3. Object manipulation testing
+  Write a test for a function called `keyValue` that checks whether an object includes a certain key-value pair. 
+  The function should take an object, a key, and a value as arguments, and return true if the key-value pair exists in the object, false otherwise. 
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  4. Async function testing
+  Write a test for a function called `getData` that fetches data from a given URL and returns the data.
+  The function should take a URL string as an argument, and return a Promise that resolves with the fetched data. 
+  You may use a library such as axios for making the HTTP request.
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  5. Mocking and spy testing
+  Write a test for a function called `myFunction` that calls another function `anotherFunction` with a specific argument.
+  You should use jest.fn() to create a mock function for `anotherFunction`, and jest.spyOn() to make sure `myFunction` calls `anotherFunction` with the correct argument.
+  Then implement the functions to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+6. Complex Array manipulation testing
+
+Write a test for a function called findMostFrequent that finds the most frequent item in an array. The function should take an array as an argument, and return the most frequent item. If there are multiple items that appear the maximum number of times, the function can return any of them. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+7. Complex Object manipulation testing
+
+Write a test for a function called deepKeyValue that checks whether a key-value pair exists anywhere within a nested object, including at any depth. The function should take an object, a key, and a value as arguments, and return true if the key-value pair exists in the object or any of its nested objects, false otherwise. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  8. Array to Object conversion testing
+
+Write a test for a function called arrayToObject that takes an array of objects, each with a key and value property, and converts it into an object with those keys and values. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  9. Object to Array conversion testing
+
+Write a test for a function called objectToArray that takes an object and returns an array of its key-value pairs, each represented as a {key: ..., value: ...} object. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  10. Array sorting testing
+
+Write a test for a function called sortArray that sorts an array in ascending order. The function should take an array as an argument, and return a new array that contains the same elements but sorted. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  11. Object sorting testing
+
+Write a test for a function called sortObjectKeys that sorts an object by its keys in alphabetical order. The function should take an object as an argument, and return a new object that contains the same key-value pairs but with keys sorted. Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
