3295540850a4237b4c0197a0313f0ad6b6d6312b
Taro Schenker <taroschenker@hotmail.co.uk>
2023-06-15 12:46:39 +0100

    added ts tests

---
+++ GenericsTDD/exercise.test.ts
@@ -0,0 +1,54 @@
+/* 
+  1. Array Filter Testing with Generics
+
+  Write a test for a function called `filterArray` that filters an array based on a condition provided as a function. 
+  The function should take an array and a condition function as arguments, and return a new array that contains elements 
+  for which the condition function returns true.
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  2. KeyValueStore Testing with Generics
+
+  Write a test for a class called `KeyValueStore`. The class should have the following methods:
+    - `set(key: string, value: T): void` : sets the value at key to value
+    - `get(key: string): T | undefined` : gets the value at key, or undefined if it does not exist
+
+  The `KeyValueStore` should be generic and work with values of any type.
+  Then implement the class to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  3. TypeMapping Testing with Generics
+
+  Write a test for a function called `mapTypes`. The function should take an array of any type and a mapping function as arguments. 
+  The mapping function should transform each element in the input array to a different type, 
+  and `mapTypes` should return a new array with the transformed elements.
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  4. CompareEntities Testing with Generics
+
+  Write a test for a function called `compareEntities`. The function should take two entities of any type and a comparator function. 
+  The comparator function should accept two parameters of the same type as the entities and return a boolean representing whether the entities are equal.
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
+
+/* 
+  5. Identity Function Testing with Generics
+
+  Write a test for a function called `identity`. The function should take a single argument and return that argument. 
+  The function should be generic and work with arguments of any type.
+  Then implement the function to make the test pass.
+*/
+
+// TODO: Write your test here
